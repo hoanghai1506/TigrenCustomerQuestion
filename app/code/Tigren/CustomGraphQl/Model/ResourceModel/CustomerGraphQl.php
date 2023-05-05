1d984adf678df8f426aa41b990d0f32e93d09433
hoanghai1506 <luonghoanghai2002@gmail.com>
2023-05-05 16:32:40 +0700

    second

--- app/code/Tigren/CustomGraphQl/Model/ResourceModel/CustomerGraphQl.php
+++ app/code/Tigren/CustomGraphQl/Model/ResourceModel/CustomerGraphQl.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2023 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
 namespace Tigren\CustomGraphQl\Model\ResourceModel;
 
 class CustomerGraphQl extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
